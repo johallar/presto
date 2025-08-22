d3a25f6b85c18c00cfad5e51d6349b055851ae12
Joe O'Hallaron <johallaron@nvidia.com>
2025-08-21 21:56:35 -0600

    Fix deps from bad merge, fix additional test that use ServerInfo

--- presto-main/src/main/java/com/facebook/presto/server/ServerInfoResource.java
+++ presto-main/src/main/java/com/facebook/presto/server/ServerInfoResource.java
@@ -22,15 +22,15 @@ import com.facebook.presto.metadata.StaticCatalogStore;
 import com.facebook.presto.spi.NodeState;
 import com.facebook.presto.sql.analyzer.FeaturesConfig;
 
-import javax.annotation.security.RolesAllowed;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 
 import java.util.Optional;
 
